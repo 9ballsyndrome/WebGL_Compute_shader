6b0001f833e0833c2e0a465ae51f62a57fdc0959
9ballsyndrome <9.ball.syndrome@gmail.com>
2019-03-24 06:37:18 +0900

    webgl-compute-vertex: Update: Simplify the structure of ssbo

--- webgl-compute-vertex/js/script.js
+++ webgl-compute-vertex/js/script.js
@@ -32,11 +32,8 @@ const script = async () => {
   // language=GLSL
   const computeShaderSource = `#version 310 es
     layout (local_size_x = 8, local_size_y = 1, local_size_z = 1) in;
-    struct Particle {
-      vec2 pos;
-    };
     layout (std430, binding = 0) buffer SSBO {
-     Particle data[];
+      vec2 pos[];
     } ssbo;
     
     uniform float time;
@@ -44,7 +41,7 @@ const script = async () => {
     void main() {
       uint threadIndex = gl_GlobalInvocationID.x;
       float floatIndex = float(threadIndex);
-      ssbo.data[threadIndex].pos = vec2(floatIndex * 0.25 - 0.875, 0.5 * sin(time * 0.02 + floatIndex * 0.5));
+      ssbo.pos[threadIndex] = vec2(floatIndex * 0.25 - 0.875, 0.5 * sin(time * 0.02 + floatIndex * 0.5));
     }
     `;
 
